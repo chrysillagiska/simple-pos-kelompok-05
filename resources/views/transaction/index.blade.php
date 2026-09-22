14b3d30d38fab9449250bc0f0ca3c5947db4f740
Hafiz Ahmad Zaim Kuntoro <hafizahmadzaimkuntoro@gmail.com>
2026-09-22 12:16:59 +0700

    tambah view riwayat transaksi

---
+++ resources/views/transaction/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'Riwayat Transaksi')
+
+@section('content')
+
+<hi class="text-lg font-semibold mb-4">
+    Riwayat Transaksi
+</hi>
+
+<div class="border rounded-md p-4">
+    <p class="text-slate-500">
+        Belum ada riwayat transaksi
+    </p>
+</div>      
+
+@endsection
