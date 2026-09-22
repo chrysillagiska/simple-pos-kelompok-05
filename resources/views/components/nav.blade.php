966b746cb57fba35fb6f7e374faf6193c6c5155c
Hafiz Ahmad Zaim Kuntoro <hafizahmadzaimkuntoro@gmail.com>
2026-09-22 11:56:08 +0700

    rapikan navigasi dan tanda halaman aktif

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,5 +1,20 @@
 <nav class="bg-slate-900 text-white px-4 py-3 flex gap-4">
-    <span class="font-semibold">Simple POS</span>
-    <a href="{{ route('pos.create') }}" class="hover:underline">Kasir</a>
-    <a href="{{ route('transactions.index') }}" class="hover:underline">Transaksi</a>
+    <div class="flex items-center gap-6">
+        <span class="font-semibold">
+            Simple POS
+        </span>
+        <a 
+            href="{{ route('pos.create') }}" 
+            class="px-3 py-2 rounded hover:bg-slate-700
+            {{ request() -> routeIs('pos.create') ? 'bg-slate-700' : ''}}">
+            Kasir
+        </a>
+        <a 
+            href="{{ route('transactions.index') }}" 
+            class="px-3 py-2 rounded hover:bg-slate-700
+            {{ request() -> routeIs('transaction.index') ? 'bg-slate-700' : ''}}">
+            Transaksi
+        </a>
+
+    </div>
 </nav>
